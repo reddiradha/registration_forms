835619a7f30756b3d9621df3880084a55b746c8f
Reddi Radha <you@example.com>
2023-05-03 17:22:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from app.forms import *
+# Create your views here.
+def registration(request):
+    ufo=UserForm()
+    pfo=ProfileForm()
+    d={'ufo':ufo,'pfo':pfo}
+    if request.method=='POST'and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            nsuo=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            nsuo.set_password(pw)
+            nsuo.save()
+
+            nspo=pfd.save(commit=False)
+            nspo.username=nsuo
+            nspo.save()
+            return HttpResponse('registration is successfull')
+        else:
+            return HttpResponse('data is not valid')
+    return render(request,'registration.html',d)
